8b35dcb57cbe1dc532decd892abf1b7cf8f18e56
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-16 23:01:14 +0100

    Update Layout.tsx

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,1 +1,26 @@
+import React, { ReactNode } from "react";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* Header */}
+      <Header />
+
+      {/* Main Content */}
+      <main className="flex-grow px-4 py-6 bg-gray-50">
+        {children}
+      </main>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
