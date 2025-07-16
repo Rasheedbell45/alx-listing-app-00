231c2d0f30cccb2bf739bdf35790c333218280f0
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-16 23:12:41 +0100

    Create properties.ts

---
+++ constants/properties.ts
@@ -0,0 +1,23 @@
+export const PROPERTYLISTINGSAMPLE = [
+  {
+    id: 1,
+    name: "Seaside Villa",
+    price: "$250/night",
+    rating: 4.8,
+    image: "/assets/villa.jpg",
+  },
+  {
+    id: 2,
+    name: "City Apartment",
+    price: "$120/night",
+    rating: 4.5,
+    image: "/assets/apartment.jpg",
+  },
+  {
+    id: 3,
+    name: "Country House",
+    price: "$180/night",
+    rating: 4.7,
+    image: "/assets/countryside.jpg",
+  },
+];
