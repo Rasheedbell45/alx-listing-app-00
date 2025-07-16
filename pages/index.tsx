8c901937a37da4ef9d524006142a8e8feee3f57f
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-16 23:01:51 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,13 @@
+import React from "react";
+import Layout from "@/components/layout/Layout";
 
+const HomePage: React.FC = () => {
+  return (
+    <Layout>
+      <h1 className="text-3xl font-bold text-center">Welcome to StayEasy</h1>
+      {/* Add more content here */}
+    </Layout>
+  );
+};
+
+export default HomePage;
